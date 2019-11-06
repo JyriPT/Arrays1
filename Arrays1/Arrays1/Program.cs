70874ff6bd8853f4858f9792d1ce0a1e0f487622
Jyri Turku <jyri.turku@student.saimia.fi>
2019-11-06 09:02:34 +0200

    Code fully functional and finished.

--- Arrays1/Arrays1/Program.cs
+++ Arrays1/Arrays1/Program.cs
@@ -21,15 +21,18 @@ namespace Arrays1
                 //Tehtävä 1 valinta
                 if (select == 1)
                 {
+                    //Alustetaan taulukot
                     decimal[] productPrice = new decimal[3] { 7.96m, 72.00m, 99.90m};
                     int[] productAmount = new int[3] { 1, 3, 2};
                     decimal[] productTotalPrice = new decimal[3];
 
+                    //Luodaan arvot productTotalPrice taulukolle
                     for (int i = 0; i < 3; i++)
                     {
                         productTotalPrice[i] = productPrice[i] * productAmount[i];
                     }
 
+                    //Tulostetaan taulukon productTotalPrice arvot
                     for (int j = 0; j < 3; j++)
                     {
                         Console.WriteLine("Tuote " + (j + 1) + ": " + productTotalPrice[j] + " euroa.");
@@ -38,26 +41,30 @@ namespace Arrays1
                 //Tehtävä 2 valinta
                 else if (select == 2)
                 {
+                    //Luodaan tarvittavat muuttujat
                     int[] iT = new int[100];
                     Random rnd = new Random();
                     int sum = 0;
 
+                    //Arvotaan taulukolle iT arvot
                     for (int i = 0; i < 100; i++)
                     {
                         iT[i] = rnd.Next(51);
                     }
 
+                    //Lasketaan taulukon iT arvot yhteen sum muuttujaan
                     for (int j = 0; j < 100; j++)
                     {
                         sum += iT[j];
                     }
+                    //Lasketaan keskiarvo
                     decimal average = Decimal.Divide(sum, 100);
 
+                    //Tulostetaan vastaukset
                     for (int k = 0; k < 100; k++)
                     {
                         Console.WriteLine(k + ". " + iT[k]);
                     }
-
                     Console.WriteLine("Average: " + average);
                 }
                 
